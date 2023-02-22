df9a1f36e12f0c846a415d5699976902d0daa715
ttuba93 <123141637+ttuba93@users.noreply.github.com>
2023-02-22 21:28:23 +0600

    Update product-details.component.ts

--- lab_4/src/app/product-details/product-details.component.ts
+++ lab_4/src/app/product-details/product-details.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
 })
-export class ProductListComponent {
-  products = products;
+export class ProductDetailsComponent {
 
-  share() {
-    window.alert('The product has been shared!');
-  }
 }
